c67ead2ec13ee7ace0a8ec8c6784b4b059a4b01d
Chelsy <chelsydhir@gmail.com>
2023-02-26 14:48:44 -0800

    Personal Portfolio

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ const projects = require('../Project Overview for Nodejs Express/data.json');
 const app = express();
 const port = 3000;
 
+
+//middlewares
 app.use(bodyParser.urlencoded({extended: true}));
 app.use("/static", express.static('public'));
 app.use('/img', express.static('public/img'));
 
 app.set('view engine', 'pug');
 
-   
 
 app.get('/', (req, res) => {
     res.render('index', {data: projects});
@@ -23,13 +24,24 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/project', (req, res) => {
-    res.render('project', {data: projects});
+app.get('/project/:id', (req, res) => {
+    const id = req.params.id;
+    res.render('project', {data: projects[id]});
 });
 
 app.get('/projects', (req, res) => {
-    res.redirect('/project');
+    res.redirect('/');
+})
+
+app.get('/projects/:id', (req, res) => {
+    const id = req.params.id;
+    res.redirect(`/project/${id}`);
 });
+
+app.use((req, res) => {
+    res.status(404);
+    res.send("<h2>Page not found"+`</br></br> Click here to go back </br></br> <a href="http://localhost:3000/">Go back!</a></h2>`);
+})
 
 app.listen(port, () =>{
     console.log(`The app is listening on ${port}`);
